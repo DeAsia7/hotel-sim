09ecbd0e3e1bf89ea3855283c9873224e16a7626
DeAsia7 <asiamallory277@gmail.com>
2025-01-14 01:47:08 -0500

    updated

--- script.js
+++ script.js
@@ -1,48 +1,89 @@
+let data=[];
+//document is better to use if im seperating the html and js
+document.getElementById("formgrab").addEventListener("submit", formInputs);
 
-let data= [];
+//make sure to put event in the () so the event in line 7 doesnt cross out
+function formInputs(event){
+    event.preventDefault() 
+    let guestinput = document.getElementById("guestName")
+    let guestName= guestinput.value;
 
-function formInputs(){
-event.preventDefault()
-    let Guestname= guestName.value;
-    let checkin= stayDuration.value;
-    let checkout= stayDuration2.value;
-    let RoomNumber= roomnumber2.value
+//inside get id() match the html id
+    let checkininput = document.getElementById("stayDuration")
+    let checkin= checkininput.value;
 
+//the input needs to match the .value
+    let checkoutinput = document.getElementById("stayDuration2")
+    let checkout= checkoutinput.value;
 
+    let RoomNumberinput = document.getElementById("roomnumber2")
+    let RoomNumber= RoomNumberinput.value;
+    
+
+console.log(RoomNumber +" " + RoomNumberinput + " " + guestinput)
     let temp= {
-        Guestname,
+        guestName,
         checkin,
         checkout,
         RoomNumber,
     }
     data.push(temp)
     renderTable()
-
+    
 }
 
 
 function renderTable(){
 
-let tableLayout= 
-`<tr>
-<th>GuestName</th>
-<th>checkin</th>
-<th>checkout</th>
-<th>RoomNumber</th>
-</tr>`
+    let tableLayout= 
+    `<tr>
+    <th>GuestName</th>
+    <th>checkin</th>
+    <th>checkout</th>
+    <th>RoomNumber</th>
+    </tr>`
+    
+    
+    for (let i=0; i<data.length; i++){ console.log(data[i])
+    
+        tableLayout+= 
+    `<tr>
+    <td>${data[i].guestName}</td>
+    <td>${data[i].checkin}</td>
+    <td>${data[i].checkout}</td>
+    <td>${data[i].RoomNumber}</td>
+    </tr>`
+    }
+    formTable.innerHTML= tableLayout
+    }
 
 
-for (let i=0; i<data.length; i++){
-    tableLayout+= 
-`<tr>
-<td>${data[i].Guestname}</td>
-<td>${data[i].checkin}</td>
-<td>${data[i].checkout}</td>
-<td>${data[i].RoomNumber2}</td>
-</tr>`
-}
-formTable.innerHTML= tableLayout
-}
- 
+
+
+
+  // const isDuplicated = array.some((item) => item.RoomNumber === RoomNumber);
+//if (isDuplicated){
+   // alert("Room Number is already taken")
+//}
+
+
+     
+   // const isDuplicate = array.some((item) => item.property ===
+    //value);
+   // if (isDuplicate) {
+   //     console.log('Duplicate detected!');
+  //  }
+
+  //  array.some((item) => condition);
+    //const validateGuest = guests.some((guest) => guest.name
+   // == 'Andrea')
+    //console.log(validateGuest) // output: false
+    //const validateGuest = guests.some((guest) => guest.name
+    //Objects and DOM Manipulation 2
+    //== 'Bob')
+    //console.log(validateGuest) // output: true
+
+
+
 
 
